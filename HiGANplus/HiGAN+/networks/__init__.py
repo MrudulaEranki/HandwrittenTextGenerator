8468255777fa3ada3115d4ed61f0055f19dd18bb
Mrudula Eranki <157816034+MrudulaEranki@users.noreply.github.com>
2026-04-01 19:35:53 +0530

    Update __init__.py

--- HiGANplus/HiGAN+/networks/__init__.py
+++ HiGANplus/HiGAN+/networks/__init__.py
@@ -1,11 +1,1 @@
-from networks.model import RecognizeModel, WriterIdentifyModel, GlobalLocalAdversarialModel
-
-all_models = {
-    'gl_adversarial_model': GlobalLocalAdversarialModel,
-    'recognize_model': RecognizeModel,
-    'identifier_model': WriterIdentifyModel
-}
-
-
-def get_model(name):
-    return all_models[name]
+# inference only]
