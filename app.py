c59837f3c4be1137ee841e3399a14062dd6d1ec4
Mrudula Eranki <157816034+MrudulaEranki@users.noreply.github.com>
2026-04-01 20:50:37 +0530

    Adjust weight in addWeighted for sharper output

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def generate_glyph(char, style_vec, sigma_mult=1.0):
     arr = (r.squeeze().cpu().numpy() + 1) / 2
     arr = (arr * 255).clip(0, 255).astype(np.uint8)
     blurred = cv2.GaussianBlur(arr.astype(np.float32), (3,3), 3)   # (3,3),3
-    sharp   = cv2.addWeighted(arr.astype(np.float32), 1.5, blurred, 0, 0)   # 1.5->2.5   -0.5->-1.5
+    sharp   = cv2.addWeighted(arr.astype(np.float32), 1.5, blurred, -0.2, 0)   # 1.5->2.5   -0.5->-1.5
     sharp   = np.clip(sharp, 0, 255).astype(np.uint8)
     p5, p95 = np.percentile(sharp, 5), np.percentile(sharp, 95)
     if p95 > p5:
